b7f40b22baa523c4878bfc66002f0a10da0f7f9b
Hullen Gonzales <hullenn@gmail.com>
2013-07-12 15:20:34 -0300

    just separating code into respective files js, css and html

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Made by Hullen Gonzales (twitter.com/hullen) */
+var wpsql = function () {
+    function setURLs(urls) {
+      var el = document.getElementById("sqlfinal"),
+        sqls = '',
+        sqlsfinal = 'Sorry, you need enter the URLs!';
+      if (urls.oldname !== '' && urls.newname !== '') {
+        sqls = 'UPDATE wp_options SET option_value = replace(option_value, "' + urls.oldname + '", "' + urls.newname + '") WHERE option_name = "home" OR option_name = "siteurl";\n';
+        sqls += 'UPDATE wp_posts SET guid = REPLACE (guid, "' + urls.oldname + '", "' + urls.newname + '");\n';
+        sqls += 'UPDATE wp_posts SET post_content = REPLACE (post_content, "' + urls.oldname + '", "' + urls.newname + '");\n';
+        sqls += 'UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, "' + urls.oldname + '", "' + urls.newname + '");';
+        sqlsfinal = sqls;
+      }
+      el.innerHTML = sqlsfinal;
+    }
+    function getValue(f) {
+      var urls = [],
+        formE = document.forms[f];
+      urls.oldname = formE['url-old'].value;
+      urls.newname = formE['url-new'].value;
+      setURLs(urls);
+    }
+    function setForm(e) {
+      var f = e.name;
+      getValue(f);
+    }
+    function publicSendForm(formE) {
+      setForm(formE);
+    }
+
+    return { sendForm : publicSendForm };
+  }();
